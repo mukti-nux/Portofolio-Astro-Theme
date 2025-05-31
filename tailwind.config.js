bd929545d5e6ee3e7e85d999b97fa1e68f4b1886
root <root@DESKTOP-HsnMkt>
2025-05-31 16:44:56 +0700

    move shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,3 +40,27 @@ module.exports = {
     },
   },
 };
+// gradient move
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        'gradient-move': 'gradientMove 5s ease-in-out infinite',
+      },
+      keyframes: {
+        gradientMove: {
+          '0%, 100%': {
+            'background-position': '0% 50%',
+          },
+          '50%': {
+            'background-position': '100% 50%',
+          },
+        },
+      },
+      backgroundSize: {
+        '200': '200% 200%',
+      },
+    },
+  },
+};
+
