5f2d427da84cf22f8b629b9efd0032aafc4fd41b
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-02 14:45:21 +0700

    preact add

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import dotenv from 'dotenv';
 
+import preact from '@astrojs/preact';
+
 dotenv.config();
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
 });
