937fa3acd47bcb81b7969180fff73da1c91f448c
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-09 13:03:28 +0700

    oke salah

--- src/components/DocumentationCard.tsx
+++ src/components/DocumentationCard.tsx
@@ -8,10 +8,10 @@ interface DocProps {
 export default function DocumentationCard({ title, description, imageUrl, link }: DocProps) {
   return (
     <div class="relative max-w-2xl mx-auto mt-16">
-      <div class="min-h-[220px] absolute inset-1 bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500 
+      <div class="absolute inset-1 bg-gradient-to-r from-cyan-400 via-purple-500 to-pink-500 
                   bg-[length:200%_200%] bg-[position:0%_50%] blur-lg opacity-80 rounded-xl z-[-1] animate-gradient-move"></div>
 
-      <div class="relative rounded-xl p-6 bg-white dark:bg-gray-900 shadow-lg transition-all duration-300 border border-transparent hover:border-[3px] hover:border-[rgb(0,255,255)] group flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6 overflow-hidden">
+      <div class="min-h-[230px] relative rounded-xl p-6 bg-white dark:bg-gray-900 shadow-lg transition-all duration-300 border border-transparent hover:border-[3px] hover:border-[rgb(0,255,255)] group flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6 overflow-hidden">
         <img
           src={imageUrl}
           alt={`thumbnail ${title}`}
