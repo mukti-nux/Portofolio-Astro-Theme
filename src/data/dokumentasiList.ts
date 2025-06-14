e469cb18bd48775f5694c2293423eaa0522853eb
root <root@DESKTOP-HsnMkt.localdomain>
2025-06-14 18:29:06 +0700

    time panjangke sitik

--- src/data/dokumentasiList.ts
+++ src/data/dokumentasiList.ts
@@ -9,7 +9,7 @@ export interface DokumentasiItem {
 const dokumentasiList: DokumentasiItem[] = [
   {
     id: "DokSekolah14062025",
-    title: "Dok. Sekolah 14062025",
+    title: "Dok. Sekolah 14062025 (pre release)",
     description: "Documentation of the Graduation ceremony and Final awards ceremony of Muhammadiyah Tempuran Junior High School",
     imageUrl: "https://github.com/mukti-nux/portof-asset/blob/main/WhatsApp%20Image%202025-06-14%20at%2016.43.32_6b159128.jpg?raw=true ",
     link: "https://seeker-project-vcl.vercel.app/index.html?id=DokSekolah14062025",
