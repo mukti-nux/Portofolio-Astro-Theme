11aa6df7a4c43bb8324b4590981d645943c7427c
root <root@DESKTOP-HsnMkt.localdomain>
2025-07-17 14:35:12 +0700

    oke najal

--- src/data/productList.ts
+++ src/data/productList.ts
@@ -1,7 +1,39 @@
 export const produkList = [
   {
     nama: "Airpod",
-    gambar: "images/airpod.png",
+    gambar: "images/product/img1.png",
+    judul: "DESIGN SLIDER",
+    judulDetail: "Airpod GHTK",
+    deskripsiSingkat: "Wireless headset dengan desain minimalis dan kualitas suara jernih.",
+    deskripsiLengkap:
+      "Cocok untuk aktivitas harian maupun olahraga. Daya tahan baterai lama, koneksi stabil Bluetooth 5.3. Tersedia kontrol sentuh dan port Type-C.",
+    spesifikasi: {
+      usedTime: "6 hours",
+      chargingPort: "Type-C",
+      compatible: "Android & iOS",
+      bluetooth: "5.3",
+      control: "Touch"
+    }
+  },
+  {
+    nama: "Airpod",
+    gambar: "images/product/img2.png",
+    judul: "DESIGN SLIDER",
+    judulDetail: "Airpod GHTK",
+    deskripsiSingkat: "Wireless headset dengan desain minimalis dan kualitas suara jernih.",
+    deskripsiLengkap:
+      "Cocok untuk aktivitas harian maupun olahraga. Daya tahan baterai lama, koneksi stabil Bluetooth 5.3. Tersedia kontrol sentuh dan port Type-C.",
+    spesifikasi: {
+      usedTime: "6 hours",
+      chargingPort: "Type-C",
+      compatible: "Android & iOS",
+      bluetooth: "5.3",
+      control: "Touch"
+    }
+  },
+  {
+    nama: "Airpod",
+    gambar: "images/product/img3.png",
     judul: "DESIGN SLIDER",
     judulDetail: "Airpod GHTK",
     deskripsiSingkat: "Wireless headset dengan desain minimalis dan kualitas suara jernih.",
