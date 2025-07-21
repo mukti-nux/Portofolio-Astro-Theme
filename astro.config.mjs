e259995bc440adb68e8062361efe43eb27f60b45
root <root@DESKTOP-HsnMkt.localdomain>
2025-07-21 20:54:02 +0700

    fix err mas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import dotenv from 'dotenv';
-import React from 'react';
-
+import react from '@astrojs/react';
 import preact from '@astrojs/preact';
 
 dotenv.config();
@@ -11,5 +10,5 @@ dotenv.config();
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), preact(), React()],
+  integrations: [tailwind(), preact(), react()],
 });
