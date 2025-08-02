77bcec5b70fad593a10d0662a3d3cbdb51550f0e
root <root@DESKTOP-HsnMkt.localdomain>
2025-08-02 16:23:53 +0700

    oke rev

--- src/data/productList.ts
+++ src/data/productList.ts
@@ -1,34 +1,23 @@
 export const produkList = [
   {
-    nama: "Halaman Pertama",
-    gambar: "images/product/halamanpertama.png",
-    judul: "FILM",
+    kategori: "Film",
     tipe: "film",
-    judulDetail: "HALAMAN PERTAMA",
-    deskripsiSingkat: "A short film about the importance of literacy.",
-    deskripsiLengkap: "Dulu ia bersinar lewat buku dan prestasi, kini tenggelam dalam dunia maya tanpa batas. Tapi harapan tak pernah benar-benar padam. Dengan cinta seorang ibu dan secuil kesadaran dari dalam diri, ia kembali menapaki jalan literasi—jalan menuju masa depan.",
-    spesifikasi: {
-      "Duration": "10 minutes",
-      "Genre": "Drama",
-      "Director": "Zumila Dewi Safitri",
-      "Rating": "-",
-      "Release": "2025"
-    }
+    items: [
+      {
+        gambar: "/images/product/halamanpertama.png",
+        judul: "HALAMAN PERTAMA",
+        nama: "Drama Remaja",
+        sinopsis: "Dulu ia bersinar lewat buku dan prestasi, kini tenggelam dalam dunia maya tanpa batas. Tapi harapan tak pernah benar-benar padam. Dengan cinta seorang ibu dan secuil kesadaran dari dalam diri, ia kembali menapaki jalan literasi—jalan menuju masa depan",
+        sutradara: "Hasan Mukti",
+        tahun: 2025,
+        durasi: "10 minute",
+        linkTrailer: "https://youtube.com/..."
+      }
+    ]
   },
   {
-    nama: "Yongnuo Yn-560 III",
-    gambar: "images/product/2.png",
-    judul: "Flash",
-    tipe: "camera",
-    judulDetail: "Yongnuo Yn-560 III",
-    deskripsiSingkat: "Compact Flash With big Guide Number.",
-    deskripsiLengkap: "lorem ips.",
-    spesifikasi: {
-      "Guide Number": "58",
-      "Flash Modes": "M, S1, S2",
-      "Zoom Range": "24-105mm",
-      "Power Source": "4x AA",
-      "Recycle Time": "3s"
-    }
-  }
-]
+    kategori: "Headset",
+    tipe: "elektronik",
+    items: [ /* ... */ ]
+  },
+];
